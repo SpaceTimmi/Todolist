7ba582901a187bb50b9ebca06fd524aa9e7935ef
Timi Ombe <timmyombe@gmail.com>
2022-10-05 01:39:30 +0100

    adding template components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello World!</p> 
-      </header>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import Logo from '../assests/home.png'
+
+const Header = () => {
+    return (
+        <div className="pg-header">
+            <img src={Logo} alt="logo"/>
+            Header
+        </div>
+    )
+}
+
+export default Header;
